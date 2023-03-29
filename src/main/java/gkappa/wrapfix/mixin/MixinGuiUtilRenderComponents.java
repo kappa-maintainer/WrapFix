53463030c0c7160fa30c18ddc2f9d719b7ce6aa2
geeky_kappa <pocketkirby1@gmail.com>
2023-03-29 13:03:01 +0800

    Cleanup

--- src/main/java/gkappa/wrapfix/mixin/MixinGuiUtilRenderComponents.java
+++ src/main/java/gkappa/wrapfix/mixin/MixinGuiUtilRenderComponents.java
@@ -1,8 +1,6 @@
 package gkappa.wrapfix.mixin;
 
 
-import com.google.common.collect.Lists;
-import gkappa.wrapfix.WrapFix;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiUtilRenderComponents;
 import net.minecraft.util.text.ITextComponent;
@@ -16,17 +14,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.ArrayList;
 import java.util.List;
 
-import static net.minecraft.client.gui.GuiUtilRenderComponents.removeTextColorsIfConfigured;
-
 @Mixin({GuiUtilRenderComponents.class})
 public class MixinGuiUtilRenderComponents {
     @Shadow
     public static String removeTextColorsIfConfigured(String text, boolean forceColor) {return "";}
     @Inject(at = @At("HEAD"), method = "splitText", cancellable = true)
-    private static void modifySplitText(ITextComponent textComponent, int maxTextLenght, FontRenderer fontRendererIn, boolean notPreserveSpace, boolean forceTextColor, CallbackInfoReturnable<List<ITextComponent>> cir) {
+    private static void modifySplitText(ITextComponent textComponent, int maxTextLength, FontRenderer fontRendererIn, boolean notPreserveSpace, boolean forceTextColor, CallbackInfoReturnable<List<ITextComponent>> cir) {
         List<ITextComponent> resultList = new ArrayList<>();
 
-        for (String str : fontRendererIn.listFormattedStringToWidth(textComponent.getFormattedText(), maxTextLenght)) {
+        for (String str : fontRendererIn.listFormattedStringToWidth(textComponent.getFormattedText(), maxTextLength)) {
             if (notPreserveSpace && str.charAt(str.length() - 1) == ' ') {
                 str = str.substring(0, str.length() - 1);
             }

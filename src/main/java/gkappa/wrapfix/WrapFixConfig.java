42f33e07cbe296e4aabafc646a0a7a9123e80039
arcade_kappa <gkappa@outlands.top>
2025-03-16 15:06:07 +0800

    Allow enable on Cleanroom

--- src/main/java/gkappa/wrapfix/WrapFixConfig.java
+++ src/main/java/gkappa/wrapfix/WrapFixConfig.java
@@ -1,11 +1,12 @@
 package gkappa.wrapfix;
 
 import net.minecraftforge.common.config.Config;
+import org.apache.commons.lang3.JavaVersion;
+import org.apache.commons.lang3.SystemUtils;
 
 @Config(modid = WrapFix.MODID, name = WrapFix.MODID)
 public class WrapFixConfig {
-    @Config.Comment("Won't work on Cleanroom")
-    public static boolean patchVanilla = true;
+    public static boolean patchVanilla = !SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_9);
     public static boolean patchIndustrialWires = true;
     public static boolean patchBetterQuesting = true;
     public static boolean patchBotania = true;

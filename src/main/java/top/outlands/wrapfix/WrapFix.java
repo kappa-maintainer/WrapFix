edf414779db126e1db2d3e31b35196b79e1c9771
arcade_kappa <gkappa@outlands.top>
2026-03-26 11:01:00 +0800

    Fix templating, update to latest Cleanroom

--- src/main/java/top/outlands/wrapfix/WrapFix.java
+++ src/main/java/top/outlands/wrapfix/WrapFix.java
@@ -1,25 +1,22 @@
 package top.outlands.wrapfix;
 
-import com.cleanroommc.client.BreakIteratorHolder;
-import com.ibm.icu.segmenter.LocalizedSegmenter;
+import com.cleanroommc.client.ICU4JInstances;
 import com.ibm.icu.segmenter.Segmenter;
 import com.ibm.icu.text.BreakIterator;
-import com.ibm.icu.util.ULocale;
-import top.outlands.wrapfix.Reference;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
 
-@Mod(modid = WrapFix.MODID, name = WrapFix.NAME, version = WrapFix.VERSION, dependencies = "required-after:cleanroom@[0.5.5-alpha,)")
+@Mod(modid = WrapFix.MODID, name = WrapFix.NAME, version = WrapFix.VERSION, dependencies = "required-after:cleanroom@[0.5.6-alpha,)")
 public class WrapFix {
-    public static final String MODID = "wrapfix";
-    public static final String NAME = "WrapFix";
+    public static final String MODID = Reference.MOD_ID;
+    public static final String NAME = Reference.MOD_NAME;
     public static final String VERSION = Reference.VERSION;
 
-    public static final BreakIterator BREAK_ITERATOR = BreakIteratorHolder.BREAK_ITERATOR;
-    public static final Segmenter SEGMENTER = LocalizedSegmenter.builder().setSegmentationType(LocalizedSegmenter.SegmentationType.LINE).setLocale(ULocale.getDefault()).build();
+    public static final BreakIterator BREAK_ITERATOR = ICU4JInstances.BREAK_ITERATOR;
+    public static final Segmenter SEGMENTER = ICU4JInstances.SEGMENTER;
     public static Logger logger;
 
     @EventHandler
